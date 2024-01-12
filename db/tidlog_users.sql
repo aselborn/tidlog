58809fe12547d30724291b5f5accae6358c47753
aselborn <aselborn@gmail.com>
2024-01-12 09:45:14 +0100

    tabeller.

---
+++ db/tidlog_users.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `tidlog_users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `reg_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_login` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ db/tidloq_jobs.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `tidlog_jobs` (
+  `JobId` int unsigned NOT NULL AUTO_INCREMENT,
+  `job_date` datetime NOT NULL,
+  `job_hour` int NOT NULL,
+  `job_fastighet` varchar(45) NOT NULL,
+  `job_description` varchar(3000) DEFAULT NULL,
+  `job_username` varchar(45) NOT NULL,
+  `saved_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`JobId`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 ;
