4e9893fec9a59ff1405a1c6608bf2133f5d007e8
Anders Selborn <Anders.Selborn.ext@toyota-industries.eu>
2024-01-15 09:05:42 +0100

    fil för docker.

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 define("DB_SERVER", "localhost");
-define("DB_USERNAME", "selborn_se");
-define("DB_PASSWORD", "2mDJxZP5");
-define("DB_NAME", "selborn_se");
+// define("DB_USERNAME", "selborn_se");
+// define("DB_PASSWORD", "2mDJxZP5");
+// define("DB_NAME", "selborn_se");
 
-// define("DB_USERNAME", "anders");
-// define("DB_PASSWORD", "lytill53");
-// define("DB_NAME", "tidlog");
+define("DB_USERNAME", "anders");
+define("DB_PASSWORD", "kalleanka");
+define("DB_NAME", "tidlog");
 
 # Connection
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
