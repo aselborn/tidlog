123b73effd7eddd292f0b8af9a1731b32036ccbc
Anders Selborn <Anders.Selborn.ext@toyota-industries.eu>
2024-02-29 15:52:43 +0100

    början på en faktura1

--- code/createpdf.php
+++ code/createpdf.php
@@ -29,6 +29,8 @@ foreach($hyra as $row)
     
     $dtdat = date_create($row["duedate"]);
     $dueDate = date_format($dtdat, "Y-m-d");
+    $fullname = $row["fnamn"] . " " . $row["enamn"];
+    $ocrNr = $row["ocr"];
 }
 
 // Instanciation of inherited class
@@ -47,14 +49,27 @@ $pdf->Text(120, 208, 'INBETALNING GIRERING / AVI');
 $pdf->SetFont('ARIAL','',8);
 $pdf->Text(20, 215, 'Följande belopp skall vara oss tillhanda senast : ' . $dueDate);
 
-$pdf->Line(135,220,135,210);
-$pdf->Line(165,220,165,210);
+$pdf->Line(135,220,135,210); //vertikal linje
+$pdf->Line(165,220,165,210); //vertikal linje
 
 $pdf->SetFont('ARIAL','B',15);
 $pdf->Text(180, 217, 'OCR');
 $pdf->Line(0,220,250,220); // en rak linje.
 
 
+$pdf->SetFont('ARIAL','',10);
+$pdf->Text(10, 230, 'Vid betalning ange följande OCR nummer');
+$pdf->SetFont('ARIAL','B',10);
+$pdf->Text(10, 235, $ocrNr);
+
+
+$pdf->Text(120, 230, 'Betalningsavsändare');
+$pdf->SetFont('ARIAL','',8);
+$pdf->Text(120, 235, $fullname);
+
+
+$pdf->Line(110,263,110,270); //vertikal linje
+//$pdf->Line(110,263,110,250); // en rak linje.
 
 
 $pdf->Line(0,270,250,270); // en rak linje.
